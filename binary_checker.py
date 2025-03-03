42495fc7315de4130b34f76375f4b80ea7727b6f
FilbertHM <74392047+FilbertHainsly@users.noreply.github.com>
2025-03-03 13:05:39 +0700

    Update binary_checker.py

--- binary_checker.py
+++ binary_checker.py
@@ -1,18 +1,21 @@
 def is_valid_binary(binary_string, valid_set):
-        if binary_string == "":
-            return True
-        
-        for s in valid_set:
-            if binary_string.startswith(s):
-                if is_valid_binary(binary_string[len(s):], valid_set):  
-                    return True
-        return False
 
+    if binary_string == "":
+        return True  # Jika string kosong (λ), maka valid
+    
+    for s in valid_set:
+        if binary_string.startswith(s):         
+            if is_valid_binary(binary_string[len(s):], valid_set): # Rekursi dengan menghapus bagian yang cocok dari binary_string
+                return True
+    
+    return False  # Jika tidak bisa dipecah sesuai S*, maka tidak valid
+
+# Himpunan S dalam ppt
 S = {"00", "10", "010", "01001"}
 
-binary_string = input("Masukkan input nilai biner: ")
+binary_string = input("Masukkan nilai biner: ")
 
 if is_valid_binary(binary_string, S):
     print("Valid")
 else:
-    print("Tidak Valid")
+    print("Not Valid")
